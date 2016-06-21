a8ed2642c691c2e6f23b2fed40137b4bed98207c
Marko Boger <marko.boger@htwg-konstanz.de>
2016-06-21 09:33:53 +0200

    updated ex_02

--- de.htwg.scala.koans/src/test/scala/de/htwg/scala/exercises/Ex02_ValAndVar.scala
+++ de.htwg.scala.koans/src/test/scala/de/htwg/scala/exercises/Ex02_ValAndVar.scala
@@ -9,12 +9,12 @@ class Ex02_ValAndVar extends KoanSuite {
     a should be(5)
 
     a = 7
-    a should be(7?)
+    a should be(7)
   }
 
   koan("vals may not be reassigned") {
     val a = 5
-    a should be(5?)
+    a should be(5)
 
     // What happens if you uncomment these lines? compilation problem as a is immutable (reassignment to val)
     // a = 7
