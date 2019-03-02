bc65e304c56160df2b8c4e15373d868e674511d7
artur-ott <artur-ott@users.noreply.github.com>
2019-03-02 17:06:09 +0100

    Update VectorSpaces.scala

--- de.htwg.scala.koans/src/test/scala/de/htwg/math/VectorSpaces.scala
+++ de.htwg.scala.koans/src/test/scala/de/htwg/math/VectorSpaces.scala
@@ -5,7 +5,7 @@ import spire.math._
 import spire.implicits._
 
 class VectorSpaces extends CodeTaskSuite("Vector Space", 2) {
-  
+}
   koan("The Scala library Spire provides a type for Vector. It allows operations like addition and scalar product") {
     	val v1 = Vector(1,5,3) + Vector(2,1,-5) 
     	v1 should be(Vector(3, 6, -2))
