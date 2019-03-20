3665e72aaac2fec4ae5f7799ebb8e32efcad1cf3
Artur Ott <artur.ott@htwg-konstanz.de>
2019-03-20 15:39:58 +0100

    art of programming ii and meta file added

---
+++ de.htwg.scala.koans/src/test/scala/art/of/programming/ii/Ch08_Loops.scala
@@ -0,0 +1,227 @@
+package art.of.programming
+
+import org.codetask.koanlib.CodeTaskSuite
+import scala.Vector
+
+class Ch08_Loops extends CodeTaskSuite("Loops", 7) {
+
+  video("The while loop", "nIGiaEf1hW8")
+  koan(""" While loop:
+    Instead of using recursion or higher order methods, scala gives you the ability to write a loop
+    which you can use to iterate over something. The while loop is a lesser used form of loop. 
+    This is because in scala the while loop is a statement not an expression. It does return Unit but 
+    you want to avoid the Type Unit in Scala.""") {
+    var i = 0
+    var sum = 0
+    while (i < 10) {
+      sum += 1
+      i += 1
+    }
+    sum should be(10)
+  }
+
+  video("The do-while Loop", "h3SIkYx8Ljw")
+  koan(""" Do while loop:
+    The do while loop is similar to the while loop. The only distinction from the while loop is that 
+    the loop will execute once at minimum because of the trailing conditional. The do while loop is the
+    least used form of loop.""") {
+    var i = 0
+    var sum = 0
+    do {
+      sum += 1
+      i += 1
+    } while (i < 10)
+    sum should be(10)
+  }
+
+  video("Ranges", "lfKCoV-O2SA")
+  koan(""" Ranges: 
+    Commonly used forms of loops create a variable which will be incremented/decremented till it surpasses
+    a certain bar, limit or something else. For this general occuring event, scala introduced something called 
+    range. Ranges are collections and thereby offer all the rich methods of the scala collection. Ranges can be
+    iterated and used in loops.
+    
+    Try writing down the right types of the first 5 statements and the actual values of the last two.""") {
+    (0 to 9).isInstanceOf[scala.collection.immutable.Range.Inclusive] should be(true)
+    (0 until 9).isInstanceOf[scala.collection.immutable.Range] should be(true)
+    Array(1, 2, 3).indices.isInstanceOf[scala.collection.immutable.Range] should be(true)
+    ('a' to 'z').isInstanceOf[scala.collection.immutable.NumericRange.Inclusive[Char]] should be(true)
+    (1.0 to 2.0 by 0.5).isInstanceOf[scala.collection.immutable.NumericRange[Double]] should be(true)
+
+    (3 to 1).toString should be("Range()")
+    (3 to 1 by -1).toString should be("Range(3, 2, 1)")
+  }
+
+  video("The for loop", "NNG6Bl8pLMQ")
+  codetask(""" Exercise: (Simple for loop)
+    Write a simple for loop that sums up each element of the provided array.""") {
+    val a = Array(1, 2, 3, 4, 5)
+    
+    def sum(a:Array[Int]):Int = {
+      //solve
+      var sum = 0
+      for (i <- a.indices) {
+        sum = sum + a(i) 
+      }
+      sum
+      //endsolve
+    }
+    
+    //test
+    sum(a) should be(15)
+
+    //endtest
+  }
+
+  video("Loop examples - Evaluate Polynoms", "6v3y9jH0wHw")
+  koan(""" For loop: 
+    The main form of loop is the for loop. They are less error prone then while loops within doing the same work.
+    For loops can be structured with using Ranges as seen in the example below. Moreover the example below shows
+    how to simplify and gain more readability when using the right functions for the job.
+    
+    Note: Polynom = coeff(0)x^2+coeff(1)x^1+coeff(2)x^0.""") {
+    val a = Array(3.0, 2.0, -5.0)
+    def evalPolynomBad(coeffs: Array[Double], x: Double): Double = {
+      var sum = 0.0
+      for (i <- coeffs.indices) {
+        sum += coeffs(i) * Math.pow(x, coeffs.length - 1 - i)
+      }
+      sum
+    }
+    evalPolynomBad(a, 1.0) should be(0.0)
+
+    def evalPolynomBetter(coeffs: Array[Double], x: Double): Double = {
+      var sum = 0.0
+      var power = 1.0
+      for (c <- coeffs.reverse) {
+        sum += c * power
+        power *= x
+      }
+      sum
+    }
+    evalPolynomBetter(a, 2.0) should be(11.0)
+  }
+
+  video("Yield", "zFHCdFV3ok4")
+  koan(""" Yield:
+    Till this point we used for loops as statements not expressions. For this exact moment Scala introduced 
+    the keyword yield. In previous examples we had to write several var declarations to make things work.
+    To prevent the use of var we can now easily add the keyword yield which will give us something back.""") {
+    val response = for (i <- 1 to 10) yield i
+    response should be(Vector(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+    val a = Array(3.0, 2.0, -5.0)
+    def evalPolyBest(coeffs: Array[Double], x: Double): Double = {
+      (for (i <- coeffs.indices) yield {
+        coeffs(i) * Math.pow(x, coeffs.length - 1 - i)
+      }).sum
+    }
+    evalPolyBest(a, 3.0) should be(28.0)
+  }
+
+  video("If guards", "hEfwLier84U")
+  koan(""" Yield If Guards:
+    In addition to conditionals seen in the chapter about conditionals you can use if guards in loops. 
+    This could be done especially when applying the yield to the for loop.
+    
+    Note: Keep in mind how often the passed function is evaluated.""") {
+    val wrongResponse = for (i <- 1 to 10) yield { if (i % 2 == 0) i * i }
+    wrongResponse should be(Vector((), 4, (), 16, (), 36, (), 64, (), 100))
+
+    val rightResponse = for (i <- 1 to 10; if i % 2 == 0) yield i * i
+    rightResponse should be(Vector(4, 16, 36, 64, 100))
+  }
+
+  video("Multiple Generators", "WseHLd1cxIs")
+  koan(""" Multiple generators: 
+    Multiple generators could be described as a nested loop. With adding additional ranges into the 
+    for loop you gain the ability to create or run multidimensional collections.""") {
+    val response = for (i <- 1 to 2; j <- 1 to 2) yield (i, j)
+    response should be(Vector((1, 1), (1, 2), (2, 1), (2, 2)))
+
+    val responseWithProduct = for (i <- 1 to 2; j <- 1 to 2) yield (i, j, i * j)
+    responseWithProduct should be(Vector((1, 1, 1), (1, 2, 2), (2, 1, 2), (2, 2, 4)))
+  }
+
+  video("Patterns in for Loops", "K7K9E7uw-lg")
+  codetask(""" Exercise: (Pattern matching with yield)
+    Write a statement which uses Pattern matching, for loop and yield to concatenate the elements of a given two tuple with the 
+    String ("_"). The result should an array with the concatenated names.""") {
+    val lst = List(("Max", "Mustermann"), ("Hans", "Werner"), ("Klaus", "Mann"))
+    
+    def combineNames( list:List[(String, String)]):List[String] = {
+    //solve
+       for ((firstname, lastname) <- lst) yield firstname + "_" + lastname
+    //endsolve
+    }
+
+    //test
+    combineNames(lst) should be(List("Max_Mustermann", "Hans_Werner", "Klaus_Mann"))
+    //endtest
+  }
+
+  video("Variable declarations", "PqoUrkvhqIo")
+  koan(""" Variable declaration in for loops:
+    Another benefit for the for loop construct is that you can declare variables into the loop header. This benefits the readability
+    and maintainability of the codebase.""") {
+    val grid = for (x <- 0 to 2; y <- 2 to 0 by -1) yield (x, y)
+    grid should be(Vector((0, 2), (0, 1), (0, 0), (1, 2), (1, 1), (1, 0), (2, 2), (2, 1), (2, 0)))
+
+    val findMax = for ((x, y) <- grid; winner = scala.math.max(x, y)) yield (winner)
+    findMax should be(Vector(2, 1, 0, 2, 1, 1, 2, 2, 2))
+  }
+
+  video("For comprehension", "r86DvO_wsZ0")
+  koan(""" For Comprehension:
+    The for loop as we know isn't a real for loop in a sense that we know from other languages.
+    In Scala the for loop is internally constructed with higher order methods like map, filter, reduce and foreach. 
+    The idea behind the loop is to offer an easy way to write a loop without using the mentioned methods directly. This
+    has the benefit of constructing loops without having to write lambda literals or clumpsy syntax. On the second hand it does provide
+    a more readable codebase.""") {
+    for (i <- 1 to 10) println(i)
+    (1 to 10).foreach(i => println(i))
+
+    (for (i <- 1 to 10) yield i * i) should be(Vector(1, 4, 9, 16, 25, 36, 49, 64, 81, 100))
+    (1 to 10).map(i => i * i) should be(Vector(1, 4, 9, 16, 25, 36, 49, 64, 81, 100))
+
+    (for (i <- 1 to 10; if i % 2 == 0) yield i * i) should be(Vector(4, 16, 36, 64, 100))
+    (1 to 10).filter(_ % 2 == 0).map(i => i * i) should be(Vector(4, 16, 36, 64, 100))
+  }
+
+  video("Multidimensional for loops", "3OgZ1hMEYTg")
+  koan(""" Multidimensial loop: 
+    Besides mutliple generators you can also construct loops with explicit nesting of for loops as seen in the example 
+    below.""") {
+    val response = for (i <- 1 to 3) yield {
+      for (j <- 1 to 3) yield i * j
+    }
+
+    response should be(Vector(Vector(1, 2, 3), Vector(2, 4, 6), Vector(3, 6, 9)))
+  }
+
+  video("Parallel for loops", "in1pyU-kb-Y")
+  koan(""" Parallel for loops:
+    Due to default technologie standards these days every computer has at least two cores. To make use of these ressources,
+    programs have to be written for parallel or concurrent execution. In Scala this is easily archived by calling the method
+    par on collections.
+    Try to figure out the type of the first statement and afterwards the result of the for loop which is run in parallel.""") {
+    (1 to 10).par.isInstanceOf[scala.collection.parallel.immutable.ParRange] should be(true)
+
+    var i = 0
+    for (j <- (1 to 1000000000).par) i += 1
+    i == 1000000000 should be(false)
+  }
+
+  video("Views", "4h6F-I0Zs0M")
+  koan(""" Views:
+    Views are an performance enhancement feature given by Scala. Each time higher order methods like map, filter or reduce are called scala will create
+    a completly new collection. If you do this multiple times you will consume a lot of memory each time. The view concept encapsulates the collection with
+    a wrapper type which is less memory consuming than creating new collections.
+    Try to figure out the type of the following statements.""") {
+
+    val numbers = Array.fill(100)(Math.random)
+    numbers.isInstanceOf[Array[Double]] should be(true)
+    numbers.map(x => x * x).filter(_ < 0.25).isInstanceOf[Array[Double]] should be(true)
+    numbers.view.map(x => x * x).filter(_ < 0.25).isInstanceOf[scala.collection.SeqView[Double, Array[Double]]] should be(true)
+  }
+}
---
+++ de.htwg.scala.koans/src/test/scala/art/of/programming/ii/Ch16_BasicObjectOrientation.scala
@@ -0,0 +1,185 @@
+package art.of.programming
+
+import org.codetask.koanlib.CodeTaskSuite
+
+class Ch16_BasicObjectOrientation extends CodeTaskSuite("Basic Object Orientation", 10) {
+  
+  video("Basics of Object-Orientation", "ILk_WBYsqec")
+  koan(""" Object Orientation:
+    Object Orientation is nothing new. Over the chapters we have used Object Orientation without even knowing.
+  
+    An easy example is: 
+  
+    val a = 5.  Even this simple statement creates an object. The Class is Int and we can now call methods
+              on top of a by invoking them with a dot. 
+    a.toString() 
+      
+    In Scala every value is an object. An Object has data and a set of functionality associated with it. Objects
+    encapsulate those two elements. Scala is one of a few languages who combine functional and object orientated concepts.""") {
+    class RationalNumber(val numerator: Int, val denominator: Int) {
+      override def toString(): String = numerator + "/" + denominator
+      def multiply(r: RationalNumber): RationalNumber = {
+        new RationalNumber(numerator * r.numerator, denominator * r.denominator)
+      }
+      def *(r:RationalNumber):RationalNumber = this.multiply(r)
+    }
+    val r1 = new RationalNumber(2, 3)
+    r1.toString should be("2/3")
+
+    val r2 = new RationalNumber(1, 2)
+    r1.multiply(r2).toString should be("2/6")
+    (r1 multiply r2).toString should be("2/6")
+    (r1 * r2).toString should be("2/6")
+  }
+
+  video("Visibility and Separating Interface from Implementation", "-GPTgSBB2E0")
+  koan(""" Visibility and Separating Interface from Implementation:
+    Classes encapsulate data and methods from the outside world. In the example
+    below you can see how to setup the class that only the parameter name is directly
+    accessable. In the second example the parameter from the class is assigned to 
+    a datamember of the class which is stored during object creation. The data from the variable
+    code and tresorCode isn't accesable at all time (encapsulated).""") {
+    class Account(val name: String, private val balance: Int)
+    val a = new Account("Svens Account", 20)
+    a.name should be("Svens Account")
+  }
+
+  codetask(""" Exercises: (Class implementation)
+    Scenario: A programmer has implemented a class Bank with a bank name and a code for the safe.
+    Sadly the code of the safe has been assigned to a member of the class which was not private. Right there we
+    have got an encapsulation problem. Plenty of other programmers are now using this implementation of the class. 
+    Is there a possibility to manage the access to this class member without breaking the code for other programmers
+    who use this class?   
+    
+    Try to figure out a way to limit or be able to handle the access to this member. Try using an additional
+    variable named mSafeCode to manage the access.""") {
+    class Bank(name: String, code: String) {
+      var safeCode = code
+    }
+
+    class AdvancedBank(name: String, code: String) {
+      //solve
+      private var mSafeCode = code
+
+      def safeCode = mSafeCode
+      def safeCode_=(code: String) {
+        mSafeCode = safeCode
+      }
+      //endsolve
+    }
+    //test
+    val ab = new AdvancedBank("AB","123")
+    ab.safeCode should be ("123")
+    //endtest
+  }
+
+  video("Operator/method overloading", "ChyIF--50MA")
+  video("Operator/method overloading", "4rbmndJrjI8")
+  koan(""" Operator overloading and special methods:
+    In Scala you can not only overload methods you can even overload operators like +-/*% and so on.
+    This comes in very handy when implementing a custom type/class. You can define basic operations like in the
+    example below an addition or subtraction of vectors. In Java this feature isn't given. You could only archive
+    this by writing methods, but it wouldn't be easy to read (or not that easy as in Scala).
+    
+    Special methods like apply, update, unary and so on are nice features in Scala which let you handle certain features
+    without writing unecessary boiler plate code. See example below.""") {
+    class Vect3(val x: Double, val y: Double, val z: Double) {
+      def +(v: Vect3) = new Vect3(x + v.x, y + v.y, z + v.z)
+      def -(v: Vect3) = new Vect3(x - v.x, y - v.y, z - v.z)
+      def unary_-() = new Vect3(-x, -y, -z)
+      def apply(index: Int): Double = index match {
+        case 0 => x
+        case 1 => y
+        case 2 => z
+      }
+
+      def print(): String = "(" + x + ", " + y + ", " + z + ")"
+    }
+
+    val v1 = new Vect3(1, 2, 3)
+    val v2 = new Vect3(3, 2, 1)
+
+    val addResult = v1 + v2
+    addResult.print should be("(4.0, 4.0, 4.0)")
+    val subResult = v1 - v2
+    subResult.print should be("(-2.0, 0.0, 2.0)")
+    val inversionResult = -v1
+    inversionResult.print() should be("(-1.0, -2.0, -3.0)")
+
+    v1(1) should be(2.0)
+
+    class MutableVect3(var x: Double, var y: Double, var z: Double) {
+      def apply(index: Int): Double = index match {
+        case 0 => x
+        case 1 => y
+        case 2 => z
+      }
+
+      def update(index: Int, value: Double): Unit = index match {
+        case 0 => x = value
+        case 1 => y = value
+        case 2 => z = value
+      }
+    }
+
+    val v3 = new MutableVect3(1, 2, 3)
+    v3(0) = 14.0
+    v3(0) should be(14.0)
+  }
+
+  video("Object declarations", "Ljf6VnunEPU")
+  codetask(""" Exercise: (Object implementation)
+    Write an object (named TestObject) which increments a member variable by 1. The starting value of the 
+    member variable should be 0.""") {
+    
+    object TestObject {
+      //solve
+      var i = 0
+      def increment {
+        i += 1
+      }
+      //endsolve
+    }
+
+    TestObject.i should be(0)
+    TestObject.increment
+    TestObject.i should be(1)
+    //test
+    TestObject.i should be(1)
+    TestObject.increment
+    TestObject.i should be(2)
+    //endtest
+  }
+
+  video("Scala object Declarations: Applications ", "0sE8cH4NRsA")
+
+  video("Scala object Declarations: Companion Objects", "X4aiuETZsZs")
+  codetask(""" Exercise: (Companion Object)
+    The provided class has an error. The Error is produced by returning a Vect3(...) instance without the keyword
+    new. 
+    Try to fix this error by using a companion object with the necessary method.
+    Tip: Array(1, 2, 3) == Array.apply(1,2,3), both result in a new Array with the elements 1,2,3.""") {
+    class Vect3(val x: Double, val y: Double, val z: Double) {
+      def +(v: Vect3) = Vect3(x + v.x, y + v.y, z + v.z)
+      def -(v: Vect3) = Vect3(x - v.x, y - v.y, z - v.z)
+      def unary_-() = Vect3(-x, -y, -z)
+      def apply(index: Int): Double = index match {
+        case 0 => x
+        case 1 => y
+        case 2 => z
+      }
+
+      def print(): String = "(" + x + ", " + y + ", " + z + ")"
+    }
+
+    object Vect3 {
+      //solve
+      def apply(x: Double, y: Double, z: Double) = new Vect3(x, y, z)
+      //endsolve
+    }
+    //test
+    val v3 = Vect3(1.0,2.0,3.0)
+    //endtest
+    
+  }
+}
